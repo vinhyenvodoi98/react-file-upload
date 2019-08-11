226f57b0f305459132ea73c7f1bbe3bd44d0bd44
hoang <vinhyenvodoi98@gmail.com>
2019-08-11 16:40:53 +0700

    add: can download file when done

--- app/src/image/Image.js
+++ app/src/image/Image.js
@@ -5,19 +5,41 @@ class Image extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      url: this.props.location.state.url
+      url: this.props.location.state.uri_result,
+      download: this.props.location.state.download
     };
+
+    this.download = this.download.bind(this);
   }
 
   onChangeData = (event) => {
     this.props.onChangeData(event.target.value, this.props.index);
   };
 
+  download = () => {
+    var url = "http://192.168.43.82:5000" + this.state.download;
+    window.open(url, "_blank");
+  };
+
   render() {
+    console.log(this.state.download);
     return (
       <div className='Image'>
+        <div className='download'>
+          <button className='button-down' onClick={this.download}>
+            <img
+              className='download-button'
+              alt='download'
+              src='https://media.istockphoto.com/vectors/download-icon-isolated-vector-vector-id844294300?b=1&k=6&m=844294300&s=170x170&h=PJYj3qPn7fw7kD3RJ69J2KCISM949mLClsi78FT_djA='
+            />
+          </button>
+        </div>
         <div className='ImageCard'>
-          <img className='Image-center' alt='img' src={this.state.url} />
+          <img
+            className='Image-center'
+            alt='img'
+            src={"http://192.168.43.82:5000" + this.state.url}
+          />
         </div>
       </div>
     );
--- app/src/input/Input.js
+++ app/src/input/Input.js
@@ -10,9 +10,10 @@ class Input extends Component {
     super(props);
     this.state = {
       data: this.props.location.state.data.data,
-      isSendDone: true,
-      uri: this.props.location.state.data.img_uri
-      //   formData: this.props.location.state.formData
+      isSendDone: false,
+      uri: this.props.location.state.data.img_uri,
+      uri_result: "",
+      download: ""
     };
     this.fieldForm = this.fieldForm.bind(this);
     this.onChangeData = this.onChangeData.bind(this);
@@ -27,7 +28,11 @@ class Input extends Component {
       })
       .then(async (response) => {
         console.log("done ", response);
-        await this.setState({ isSendDone: true });
+        await this.setState({
+          isSendDone: true,
+          uri_result: response.data.upload,
+          download: response.data.download
+        });
       })
       .catch((error) => {
         console.log(error);
@@ -38,11 +43,9 @@ class Input extends Component {
     var data = this.state.data;
     data[index].field = value;
     this.setState({ data });
-    console.log(this.state.data);
   }
 
   render() {
-    console.log(this.state.uri);
     return (
       <div className='Input'>
         <div className='InputCard'>
@@ -53,23 +56,24 @@ class Input extends Component {
             <InputLable key={index} data={lable} index={index} onChangeData={this.onChangeData} />
           ))}
           <div className='upload-file-button-area'>
+            <button onClick={this.fieldForm} className='btn btn-primary'>
+              Submit
+            </button>
             {this.state.isSendDone ? (
               <Link
                 className='image'
                 to={{
                   pathname: "/image",
                   state: {
-                    url: this.state.url
+                    uri_result: this.state.uri_result,
+                    download: this.state.download
                   }
                 }}>
-                Input
+                Show result
               </Link>
             ) : (
               <div />
             )}
-            <button onClick={this.fieldForm} className='btn btn-primary'>
-              Submit
-            </button>
           </div>
         </div>
       </div>
